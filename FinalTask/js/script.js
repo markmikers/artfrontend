0fc376b97b24c1de997a778fd94b64a9390e79f2
markmikers <markfedorov93@gmail.com>
2016-08-22 03:14:20 +0300

    Final task JSON link edited & cosmetic truncating & autoprefixer work

--- FinalTask/js/script.js
+++ FinalTask/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   $spinner = $preloader.find('.spinner');
   $spinner.fadeOut();
   $preloader.delay(350).fadeOut('slow');
-  $.getJSON('./json/echo.json', function(json) {
+  $.getJSON('https://raw.githubusercontent.com/markmikers/artfrontend/master/FinalTask/json/echo.json', function(json) {
     $('.table-length h3').append("<span><strong>" + json.values.length + "</strong></span>");
     for (i = 0; i < json.values.length; i++) {
       $('table > tbody').append('<tr><td>' + json.values[i].name.v + '</td><td>' + json.values[i].firstName.v + '</td><td>' + (+(Date.now() - Date.parse(json.values[i].birthDate.v))/1000/3600/24/365).toString().slice(0, 2) + ' (' + json.values[i].birthDate.v.slice(0, 10) + ')' + '</td><td>' + '<a href="callto:' + json.values[i].tel.v + '">' + json.values[i].tel.v + '</a></td><td>' + '<a href="https://www.google.com/maps/place/' + json.values[i].address.v.replace(/ /g, "+") + '" target="_blank">' + json.values[i].address.v + '</a></td></tr>');
